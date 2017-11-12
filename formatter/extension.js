02c382642dc9339fe27e516aa2f55b06a6473104
Matthew Blode <m.blode@gmail.com>
2017-11-12 16:03:58 +1100

    format selection WIP

--- formatter/extension.js
+++ formatter/extension.js
@@ -1,97 +1,75 @@
 const vscode = require('vscode');
 const prettydiff = require('./libs/prettydiff');
 
-const dumpError = e => {
-    if (e) console.log('beautify err:', e);
-};
-
-const beautifyDocRanges = (doc, ranges, type, formattingOptions) => {
-    if (!doc) {
-        vscode.window.showInformationMessage(
-            "Beautify can't get the file information because the editor won't supply it. (File probably too large)"
+const prettyDiff = (document, range, options) => {
+    if (range === null) {
+        const start = new vscode.Position(0, 0);
+        const end = new vscode.Position(
+            document.lineCount - 1,
+            document.lineAt(document.lineCount - 1).text.length
         );
-        throw '';
+        range = new vscode.Range(start, end);
     }
-    return Promise.resolve(type ? type : 'auto').then(config =>
-        Promise.all(
-            ranges.map(range =>
-                prettydiff({
-                    source: doc.getText(range),
-                    lang: type,
-                    mode: 'beautify',
-                    langdefault: 'markup',
-                    newline: true,
-                    objsort: 'none',
-                    unformatted: true,
-                    wrap: 0,
-                    methodchain: 'chain',
-                    html: true,
-                    Twig: true,
-                })
-            )
-        )
-    );
-};
 
-const documentEdit = (range, newText) => [
-    vscode.TextEdit.replace(range, newText)
-];
+    const result = [];
+    const content = document.getText(range);
 
-const fullRange = doc =>
-    doc.validateRange(
-        new vscode.Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE)
-    );
-
-function fullEdit(type, doc, formattingOptions) {
-    let name = doc.fileName;
-    let base = vscode.workspace.rootPath || '';
-    if (base && name.startsWith(base)) name = path.relative(base, name);
-
-    const rng = fullRange(doc);
-    return beautifyDocRanges(doc, [rng], type, formattingOptions).then(
-        newText => documentEdit(rng, newText[0]),
-        dumpError
-    );
-}
-
-const applyEdits = (editor, ranges, edits) => {
-    if (ranges.length !== edits.length) {
-        console.log('FAILED:', ranges.length, edits.length, ':failed');
-        vscode.window.showInformationMessage(
-            "Beautify ranges didin't get back the right number of edits"
-        );
-        throw '';
-    }
-    return editor.edit(editorEdit => {
-        for (let i = 0; i < ranges.length; i++) {
-            editorEdit.replace(ranges[i], edits[i]);
-        }
+    var newText = prettydiff({
+        source: content,
+        lang: 'auto',
+        mode: 'beautify',
+        langdefault: 'markup',
+        newline: true,
+        objsort: 'none',
+        unformatted: true,
+        wrap: 0,
+        methodchain: 'chain',
+        html: true,
+        Twig: true
     });
+
+    result.push(vscode.TextEdit.replace(range, newText));
+    return result
 };
 
 function activate(context) {
-    vscode.languages.registerDocumentFormattingEditProvider('twig', {
-        provideDocumentFormattingEdits: function(document) {
-            const active = vscode.window.activeTextEditor;
-            if (!active || !active.document) return;
+    const active = vscode.window.activeTextEditor;
+    if (!active || !active.document) return;
 
-            const type = 'auto';
-            let ranges = [];
+    registerDocType('twig');
 
-            ranges = [fullRange(active.document)];
+    function registerDocType(type) {
+        context.subscriptions.push(
+            vscode.languages.registerDocumentFormattingEditProvider(type, {
+                provideDocumentFormattingEdits: function(
+                    document,
+                    options,
+                    token
+                ) {
+                    return prettyDiff(document, null, options);
+                }
+            })
+        );
 
-            if (ranges.length) {
-                return beautifyDocRanges(active.document, ranges, type).then(
-                    edits => applyEdits(active, ranges, edits),
-                    dumpError
-                );
-            } else return Promise.resolve();
-        }
-    });
+        context.subscriptions.push(
+            vscode.languages.registerDocumentRangeFormattingEditProvider(type, {
+                provideDocumentRangeFormattingEdits: function(
+                    document,
+                    range,
+                    options,
+                    token
+                ) {
+                    let start = document.offsetAt(range.start);
+                    let end = document.offsetAt(range.end);
+                    return prettyDiff(
+                        document,
+                        new vscode.Range(start, end),
+                        options
+                    );
+                }
+            })
+        );
+    }
 }
 
 exports.activate = activate;
-
-// this method is called when your extension is deactivated
-function deactivate() {}
-exports.deactivate = deactivate;
